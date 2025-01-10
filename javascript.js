6d1a619fdc9368b192a9606855780d73735d2f19
killerGG33 <killerbal007@gmail.com>
2025-01-10 17:25:23 +0600

    meaningful comment

--- javascript.js
+++ javascript.js
@@ -2,3 +2,11 @@ const searchBtn = document.getElementById('search-btn');
 const searchInput = document.getElementById('search-input');
 const mealResults = document.getElementById('meal-results');
 const showAllBtn = document.getElementById('show-all-btn');
+let allMeals = [];
+
+searchBtn.addEventListener('click', searchMeals);
+showAllBtn.addEventListener('click', showAllMeals);
+
+function searchMeals() {
+    const query = searchInput.value.trim();
+    if (query === '') return;
