fb97761e8aca1f195a4b2548f9918576166e367a
killerGG33 <killerbal007@gmail.com>
2025-01-10 17:21:56 +0600

    meaningful comment

---
+++ javascript.js
@@ -0,0 +1,4 @@
+const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search-input');
+const mealResults = document.getElementById('meal-results');
+const showAllBtn = document.getElementById('show-all-btn');
